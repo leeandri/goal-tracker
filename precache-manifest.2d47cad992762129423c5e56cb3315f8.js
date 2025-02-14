c29abeac465bf481ac1bdfb99f65499e019f7cec
leeandri <rl.tsirih@gmail.com>
2025-02-14 18:07:59 +0300

    Updates

---
+++ precache-manifest.2d47cad992762129423c5e56cb3315f8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7409254c80ecba003e3bfad5fb8f42c",
+    "url": "/goal-tracker/index.html"
+  },
+  {
+    "revision": "80eb3918268dff27d32b",
+    "url": "/goal-tracker/static/css/main.0910f06a.chunk.css"
+  },
+  {
+    "revision": "bfcd5a4c02064c8efa67",
+    "url": "/goal-tracker/static/js/2.810a3ca9.chunk.js"
+  },
+  {
+    "revision": "80eb3918268dff27d32b",
+    "url": "/goal-tracker/static/js/main.632ff3d9.chunk.js"
+  },
+  {
+    "revision": "86de57f6350bf4bff0ec",
+    "url": "/goal-tracker/static/js/runtime~main.3e6f99f9.js"
+  }
+]);
